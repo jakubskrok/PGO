2230e526683dfc4c7a096de2e53e9372ba58948c
jakubskrok <61780385+jakubskrok@users.noreply.github.com>
2020-05-14 00:56:33 +0200

    Update Main.java

--- Tut9_(1)/src/Main.java
+++ Tut9_(1)/src/Main.java
@@ -7,11 +7,15 @@ public class Main {
 
 
         System.out.println("Brand: " + c1.getBrand() +  " , " + "Model: " + c1.getModel() + " , " + "Type: " + c1.getType() + " , " + "Color: " + c1.getColor() + " , " + "Seats: " + c1.getNumberOfSeats());
-        System.out.println(c1.getBrand() + ": " + c1.getStart() + " or " + c1.getStop());
+        System.out.println(c1.getBrand() + ": ");
+        c1.setStart("");
+        c1.setStop("");
         System.out.println(" ");
         System.out.println(" ");
         System.out.println("Brand: " + c2.getBrand() +  " , " + "Model: " + c2.getModel() + " , " + "Type: " + c2.getType() + " , " + "Color: " + c2.getColor() + " , " + "Seats: " + c2.getNumberOfSeats());
-        System.out.println(c2.getBrand() + ": " + c2.getStart() + " or " + c2.getStop());
+        System.out.println(c2.getBrand() + ": ");
+        c2.setStart("");
+        c2.setStop("");
 
 
     }
