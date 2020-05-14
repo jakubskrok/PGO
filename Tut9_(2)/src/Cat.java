4b6a4d8d9603280d2970c301377048032fdf219f
jakubskrok <61780385+jakubskrok@users.noreply.github.com>
2020-05-14 14:46:04 +0200

    Update Cat.java

--- Tut9_(2)/src/Cat.java
+++ Tut9_(2)/src/Cat.java
@@ -11,6 +11,6 @@ public class Cat extends Feline {
     @Override
     public void setMakeNoise(String makeNoise) {
         this.makeNoise = makeNoise;
-        System.out.println("Make noise");
+        System.out.println("Meowing");
     }
 }
